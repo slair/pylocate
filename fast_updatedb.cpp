6825470862569bfc7afaade50c1f8a9fdba655b1
slair <nobody@nowhere.com>
2022-01-25 14:00:00 +0400

    autocommit

--- fast_updatedb.cpp
+++ fast_updatedb.cpp
@@ -1,38 +1,63 @@
 // -*- coding: utf-8 -*-
 
-#include <iostream>
-
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#include <iostream>
+
+#include <QDebug>
+#include <QDir>
+
+int traverse( const char* start_folder )
+{
+	std::cout << "start_folder = " << start_folder << std::endl;
+
+    QDir currentDir = start_folder;
+	std::string se;
+
+    currentDir.setFilter( QDir::AllDirs|QDir::AllEntries|QDir::Hidden|
+		QDir::System|QDir::NoDotAndDotDot );
+
+    QStringList entries = currentDir.entryList();
+    for( QStringList::ConstIterator entry=entries.begin();
+			entry!=entries.end();
+			++entry) {
+
+		se = entry->toStdString();
+		std::cout << entry->toStdString() << std::endl;
+
+		// qDebug().nospace() << qPrintable(*entry);
+    }
+    return 0;
+}
+
+
 static PyObject* updatedb_impl(PyObject* self, PyObject* args) {
 	const char* start_folder;
-	int res;
 
 	std::cout << "updatedb_impl:" << std::endl;
 
 	if (!PyArg_ParseTuple(args, "s", &start_folder))
         return NULL;
 
-	std::cout << "start_folder = " << start_folder << std::endl;
+	traverse(start_folder);
 
-    res = 1234;	// system(command);
-    // return PyLong_FromLong(res);
 	Py_RETURN_NONE;
 }
 
 
 static PyMethodDef CPPMethods[] = {
     {"fast_updatedb",  updatedb_impl, METH_VARARGS, "Update DB"},
-    {NULL, NULL, 0, NULL}        /* Страж */
+    {NULL, NULL, 0, NULL}	/* Страж */
 };
 
 static struct PyModuleDef cppmodule = {
     PyModuleDef_HEAD_INIT,
-    "cpp",   /* имя модуля */
-    NULL, /* документация модуля, может иметь значение NULL */
-    -1,       /* размер состояния модуля для каждого интерпретатора, или -1, если
-                 модуль сохраняет состояние в глобальных переменных. */
+    "cpp",	/* имя модуля */
+    NULL,	/* документация модуля, может иметь значение NULL */
+    -1,		/* размер состояния модуля для каждого интерпретатора,
+				или -1, если
+				модуль сохраняет состояние в глобальных переменных. */
     CPPMethods
 };
 
--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@
 
 from distutils.core import setup, Extension
 
-module1 = Extension('cpp',
-                    sources = ['fast_updatedb.cpp'])
+module1 = Extension(
+	'cpp',
+	define_macros = [("MAJOR_VERSION", "1"), ("MINOR_VERSION", "0")],
+	include_dirs = [
+		"C:\\apps\\qt\\include",
+		"C:\\apps\\qt\\include\\QtCore"
+	],
+	libraries = ["Qt5Core"],
+	library_dirs = ["C:\\apps\\qt\\lib"],
+	sources = ["fast_updatedb.cpp"])
 
-setup (name = 'FastUpdateDB',
-       version = '1.0',
-       description = 'Update DB with C++',
-       ext_modules = [module1])
+setup (name = "FastUpdateDB",
+	   version = "1.0",
+	   description = "Update DB with C++ and Qt5",
+	   ext_modules = [module1])
