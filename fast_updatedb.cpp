398e6316d6096ad63066bc6c1f80015e7f740399
slair <slair@users.noreply.github.com>
2022-02-06 00:20:58 +0400

    autocommit

--- fast_updatedb.cpp
+++ fast_updatedb.cpp
@@ -8,38 +8,80 @@
 #include <QDebug>
 #include <QDir>
 
+
+PyObject* cfgObj;
+QStringList cfg_exclude_folders;
+
+
 int traverse( const char* start_folder )
 {
 	std::cout << "start_folder = " << start_folder << std::endl;
 
-    QDir currentDir = start_folder;
-	std::string se;
+	QStringList paths;
+	paths << start_folder;
 
-    currentDir.setFilter( QDir::AllDirs|QDir::AllEntries|QDir::Hidden|
-		QDir::System|QDir::NoDotAndDotDot );
+	while (!paths.isEmpty()) {
 
-    QStringList entries = currentDir.entryList();
-    for( QStringList::ConstIterator entry=entries.begin();
-			entry!=entries.end();
-			++entry) {
+		QDir currentDir = paths.constFirst();
+		paths.removeFirst();
 
-		se = entry->toStdString();
-		std::cout << entry->toStdString() << std::endl;
-		// std::cout << qPrintable(*entry) << std::endl;
+		currentDir.setFilter( QDir::AllDirs|QDir::AllEntries|QDir::Hidden|
+			QDir::System|QDir::NoDotAndDotDot );
 
-		// qDebug().nospace() << qPrintable(*entry);
-    }
+		QFileInfoList entries = currentDir.entryInfoList();
+		for( QFileInfoList::ConstIterator entry=entries.begin();
+				entry!=entries.end(); entry++) {
+			QString it;
+			if (entry->isFile()) {
+				it = "file ";
+			} else if (entry->isJunction()) {
+				it = "junction ";
+			} else if (entry->isSymbolicLink()) {
+				it = "symboliclink ";
+			} else if (entry->isDir()) {
+				it = "dir ";
+				QString new_dir = QDir::toNativeSeparators(
+									QDir::cleanPath(
+										currentDir.absolutePath() +
+										QDir::separator() +
+										entry->fileName()
+									));
+				if (!cfg_exclude_folders.contains(new_dir)) {
+					//~ paths << new_dir;
+				} else {
+					std::cout << "Folder '" <<
+						new_dir.toStdString() << "' excluded by config" <<
+						std::endl;
+				}
+			} else {
+			}
+			std::cout << it.toStdString() <<
+				entry->fileName().toStdString() << std::endl;
+		}
+	}
     return 0;
 }
 
 
 static PyObject* updatedb_impl(PyObject* self, PyObject* args) {
+
 	const char* start_folder;
 
-	std::cout << "updatedb_impl:" << std::endl;
+	// std::cout << "updatedb_impl:" << std::endl;
 
-	if (!PyArg_ParseTuple(args, "s", &start_folder))
+	if (!PyArg_ParseTuple(args, "sO", &start_folder, &cfgObj))
         return NULL;
+
+	PyObject* ef_pylistObj = PyObject_GetAttrString(cfgObj, "exclude_folders");
+	Py_ssize_t ef_size = PyList_Size(ef_pylistObj);
+
+	for(Py_ssize_t i=0; i<ef_size; i++) {
+		PyObject* ef_pystr = PyList_GetItem(ef_pylistObj, i);
+		const char *c_str = PyUnicode_AsUTF8(ef_pystr);
+		QString out = QString(c_str);
+		cfg_exclude_folders << QDir::toNativeSeparators(QDir::cleanPath(out));
+	}
+	qDebug() << "cfg_exclude_folders =" << cfg_exclude_folders;
 
 	traverse(start_folder);
 
@@ -62,6 +104,6 @@ static struct PyModuleDef cppmodule = {
     CPPMethods
 };
 
-PyMODINIT_FUNC PyInit_cpp(void) {
+PyMODINIT_FUNC PyInit_cpp(void) {	// cppcheck: disable=unusedFunction
     return PyModule_Create(&cppmodule);
 }
